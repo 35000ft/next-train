5788786f170ac654a3105ee8191589e747694450
35000ft <above35000ft@foxmail.com>
2023-09-24 10:46:29 +0800

    v0.1.1

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 export async function fetchStationTrainInfo(stationId, lineId) {
-    const url = `http://localhost/nj-metro-realtime/station/train-info/${stationId}/${lineId}`
-    // const url = `https://api.35000ft.top/nj-metro-realtime/station/train-info/${stationId}/${lineId}`
+    // const url = `http://localhost/nj-metro-realtime/station/train-info/${stationId}/${lineId}`
+    const url = `https://api.35000ft.top/nj-metro-realtime/station/train-info/${stationId}/${lineId}`
     return await axios.post(url).then(res => {
         return res.data.data
     }, () => {
