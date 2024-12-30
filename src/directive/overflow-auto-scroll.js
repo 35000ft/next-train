4eae099b076a33c7cbac14696ed27f91948427e1
chenchunlin <chunlin.chen@cato-chem.com>
2024-12-30 14:51:30 +0800

    fix overflow-auto-scroll flex element

--- src/directive/overflow-auto-scroll.js
+++ src/directive/overflow-auto-scroll.js
@@ -1,4 +1,6 @@
 // overflow-auto-scroll.js
+import _ from "lodash";
+
 export default {
     mounted(el) {
         // Check content if width over the container size
@@ -7,19 +9,23 @@ export default {
             let contentWidth = el.scrollWidth;  // Actual width of content
             if (el.style.display === 'flex') {
                 // If element is display as flex, the actual width is not the scrollWidth but the sum width of sub elements
-                const childNodes = el.children;
-                contentWidth = 0;
-                for (const child of childNodes) {
-                    const computedStyle = window.getComputedStyle(child)
-                    const marginLeft = parseFloat(computedStyle.marginLeft)
-                    const marginRight = parseFloat(computedStyle.marginRight)
-                    contentWidth += child.offsetWidth + marginLeft + marginRight;
-                }
-                if (contentWidth < el.parentElement.clientWidth) {
-                    el.style.width = el.parentElement.clientWidth + 'px'
-                } else {
-                    el.style.width = contentWidth - 1 + 'px'
-                }
+                setTimeout(() => {
+                    const childNodes = el.children;
+                    contentWidth = 0;
+                    for (const child of childNodes) {
+                        const computedStyle = window.getComputedStyle(child)
+                        const marginLeft = parseFloat(computedStyle.marginLeft)
+                        const marginRight = parseFloat(computedStyle.marginRight)
+                        contentWidth += child.offsetWidth + marginLeft + marginRight;
+                    }
+                    if (contentWidth < el.parentElement.clientWidth) {
+                        el.classList.remove("auto-scroll-content");
+                    } else {
+                        el.style.width = contentWidth + 'px'
+                        el.classList.add("auto-scroll-content");
+                    }
+                }, _.random(80, 150))
+                return
             }
             if (contentWidth > el.offsetWidth) {
                 el.classList.add("auto-scroll-content");
