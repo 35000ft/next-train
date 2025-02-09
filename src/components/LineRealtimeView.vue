0864e95fea2232003d60fe22819b2b2adcf7a83a
35000ft <above35000ft@foxmail.com>
2025-02-10 00:34:22 +0800

    S1

--- src/components/LineRealtimeView.vue
+++ src/components/LineRealtimeView.vue
@@ -7,6 +7,7 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import {useStore} from "vuex";
+import {drawRoundedLShape, drawRoundedRect} from "src/utils/canvas-utils";
 
 const store = useStore()
 
@@ -38,6 +39,16 @@ const drawConfig = {
             "type": "BRANCH",
             "direction": "DOWN-RIGHT"
         },
+        {
+            "lineId": "53",
+            "type": "BRANCH",
+            "direction": "DOWN-LEFT"
+        },
+        {
+            "lineId": "5",
+            "type": "INTERSECTION",
+            "direction": "RIGHT"
+        },
     ]
 }
 
@@ -61,36 +72,59 @@ async function drawMetroLine(canvas, config) {
     if (!canvas || !config) return;
     const ctx = canvas.getContext("2d");
 
-    function drawLine(_ctx, color, width, from, to) {
-        _ctx.beginPath();
-        _ctx.moveTo(from.x, from.y);
-        _ctx.lineTo(to.x, to.y);
-        _ctx.strokeStyle = color;
-        _ctx.lineWidth = width;
-        _ctx.stroke();
+    function drawLine(_ctx, lineInfo, width, from, to) {
+        const {color, name} = lineInfo
+        _ctx.beginPath()
+        _ctx.moveTo(from.x, from.y)
+        _ctx.lineTo(to.x, to.y)
+        _ctx.strokeStyle = color
+        _ctx.lineWidth = width
+        _ctx.stroke()
     }
 
-    function drawRoundedLShape(_ctx, color, lineWidth, from, to) {
-        const cornerRadius = 30; // 圆角半径
-        const isRight = to.x > from.x; // 是否向右绘制
-        const midX = isRight ? from.x + cornerRadius : from.x - cornerRadius;
-        const midY = to.y - cornerRadius;
-        console.log('min', midX, midY, cornerRadius)
+    function drawLineName(_ctx, lineInfo, position) {
+        const {color, name} = lineInfo
+        // 计算文本框位置
+        const textPadding = 4;
+        const fontSize = 10;
+        const radius = 9;
+
+        _ctx.font = `${fontSize}px Arial`;
+        _ctx.textBaseline = "middle";
+        _ctx.textAlign = "center";
+
+        const textWidth = _ctx.measureText(name).width + textPadding * 4;
+        const textHeight = fontSize + textPadding * 2;
+
+        const isLeft = position.x === 0
+        // 矩形框位置：在 `to` 点上方
+        const rectX = isLeft ? 1 : position.x - textWidth;
+        const rectY = isLeft ? position.y + 10 : position.y - textHeight - 10; // 适当上移，避免重叠
+        // 画圆角矩形
         _ctx.beginPath();
-        _ctx.moveTo(from.x, from.y);
-
-        // 画竖线
-        _ctx.lineTo(from.x, midY);
-
-        // 画圆角
-        _ctx.arcTo(from.x, to.y, midX, to.y, cornerRadius);
-        // 画横线
-        _ctx.moveTo(midX, to.y);
-        _ctx.lineTo(to.x, to.y);
-
+        _ctx.lineWidth = 2;
         _ctx.strokeStyle = color;
-        _ctx.lineWidth = lineWidth;
+        _ctx.fillStyle = color;
+        drawRoundedRect(_ctx, rectX, rectY, textWidth, textHeight, radius);
+        _ctx.fill();
         _ctx.stroke();
+
+        // 填充文本
+        _ctx.fillStyle = '#ffffff';
+        _ctx.fillText(name, isLeft ? textWidth / 2 : position.x - textWidth / 2, rectY + textHeight / 2);
+    }
+
+    function drawStationName(_ctx, station, position) {
+        const {name} = station
+        const fontSize = 14;
+
+        _ctx.font = `${fontSize}px Arial`;
+        _ctx.textBaseline = "middle";
+        _ctx.textAlign = "left";
+
+        // 填充文本
+        _ctx.fillStyle = 'black';
+        _ctx.fillText(name, position.x, position.y);
     }
 
     function drawStation(_ctx, x, y, color, isHalf = false) {
@@ -103,9 +137,9 @@ async function drawMetroLine(canvas, config) {
         _ctx.stroke();
     }
 
-    const lineData = await lineInfoLoader(config.lineId);
-    const stations = lineData.stations;
-    const lineColor = lineData.color;
+    const lineInfo = await lineInfoLoader(config.lineId);
+    const stations = lineInfo.stations;
+    const lineColor = lineInfo.color;
     const padding = 50;
     const width = canvas.width / 2;
     const lineWidth = 10;
@@ -155,54 +189,77 @@ async function drawMetroLine(canvas, config) {
     canvas.height = lineHeight + padding;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    const stationCirclesToDraw = []
 
     // **绘制支线**
     if (config.extraLines) {
-        for (const extra of config.extraLines) {
-            const extraLineData = extraLineDataMap[extra.lineId];
-            const extraStations = extraLineData.stations;
-            const extraColor = extraLineData.color;
+        for (const extraLineConfig of config.extraLines) {
+            const extraLineInfo = extraLineDataMap[extraLineConfig.lineId];
+            const extraStations = extraLineInfo.stations;
+            const extraColor = extraLineInfo.color;
 
             // 找到交汇站点
-            const intersection = extraStations.find(s => stationPositions[s.id]);
-            if (!intersection) continue;
+            const intersectionStation = extraStations.find(s => stationPositions[s.id]);
+            if (!intersectionStation) continue;
 
-            const intersectionPoint = stationPositions[intersection.id];
+            const intersectionPoint = stationPositions[intersectionStation.id];
 
             // 计算支线终点坐标
-            const branchStations = getBranchStations(extraStations, intersection.id, extra.showAll);
-            const branchEndPosition = calcBranchEndPosition(intersectionPoint, branchStations, extra.type, SEGMENT_LENGTH, extra.direction, canvas);
+            const branchStations = getBranchStations(extraStations, intersectionStation.id, extraLineConfig.showAll);
+            const branchEndPosition = calcBranchEndPosition(intersectionPoint, branchStations, extraLineConfig.type, SEGMENT_LENGTH, extraLineConfig.direction, canvas);
 
             if (!branchEndPosition) continue;
 
             // 画支线
-            if (extra.type === 'TROUGH') {
-                drawLine(ctx, extraColor, lineWidth, intersectionPoint, branchEndPosition);
-            } else {
+            if (extraLineConfig.type === 'TROUGH') {
+                drawLine(ctx, extraLineInfo, lineWidth, intersectionPoint, branchEndPosition);
+                drawLineName(ctx, extraLineInfo, {x: canvas.width, y: intersectionPoint.y + 30})
+                drawLineName(ctx, lineInfo, {x: canvas.width, y: intersectionPoint.y + 10})
+            } else if (extraLineConfig.type === 'BRANCH') {
                 //画一条圆角L形 终点位置为branchEndPosition 是一个对象{x,y}
                 drawRoundedLShape(ctx, extraColor, lineWidth, intersectionPoint, branchEndPosition);
+                drawLineName(ctx, extraLineInfo, branchEndPosition)
+            } else if (extraLineConfig.type === 'INTERSECTION') {
+                if (!extraLineConfig.direction) {
+                    drawLine(ctx, extraLineInfo, lineWidth, {x: 0, y: intersectionPoint.y}, {
+                        x: canvas.width,
+                        y: intersectionPoint.y
+                    })
+                } else {
+                    drawLine(ctx, extraLineInfo, lineWidth, intersectionPoint, branchEndPosition)
+                }
+                drawLineName(ctx, extraLineInfo, branchEndPosition)
             }
             // 画支线站点
             branchStations.forEach((station, i) => {
-                drawStation(ctx, mainLineX, intersectionPoint.y + i * SEGMENT_LENGTH, extraColor);
+                const stationY = intersectionPoint.y + i * SEGMENT_LENGTH
+                stationCirclesToDraw.push([mainLineX, stationY, extraColor])
+                if (extraLineConfig.type === 'TROUGH') {
+                    drawStationName(ctx, station, {x: 0, y: stationY + 16})
+                }
             })
         }
     }
 
     // **绘制主线**
-    drawLine(ctx, lineColor, lineWidth, {x: width, y: padding}, {
+    drawLine(ctx, lineInfo, lineWidth, {x: width, y: padding}, {
         x: width,
         y: padding + (stations.length - 1) * SEGMENT_LENGTH
-    });
+    })
 
+    stationCirclesToDraw.forEach(params => {
+        drawStation(ctx, ...params)
+    })
     // **绘制主线站点**
-    stations.forEach(({id}, index) => {
-        const isIntersection = intersections.has(id)
-        drawStation(ctx, width, padding + index * SEGMENT_LENGTH, lineColor, isIntersection);
+    stations.forEach((station, index) => {
+        const isIntersection = intersections.has(station.id)
+        const stationY = padding + index * SEGMENT_LENGTH
+        drawStation(ctx, width, stationY, lineColor, isIntersection);
+        drawStationName(ctx, station, {x: 0, y: stationY + 16})
     });
 }
 
-function calcBranchEndPosition(intersectionPoint, branchStations, positionType, segmentLength, direction = "", canvas) {
+function calcBranchEndPosition(intersectionPoint, branchStations, positionType, segmentLength, direction, canvas) {
     const {x, y, index} = intersectionPoint
     const endPosition = {}
     if (positionType === 'TROUGH') {
@@ -213,7 +270,7 @@ function calcBranchEndPosition(intersectionPoint, branchStations, positionType, 
             endPosition.y = y + segmentLength * (branchStations.length - 1)
         }
     } else if (positionType === 'BRANCH') {
-        let [yDirection, xDirection] = direction.split('-')
+        let [yDirection, xDirection] = (direction && direction.split('-')) || [null, null]
         if (!['DOWN', 'UP'].includes(yDirection)) {
             yDirection = 'DOWN'
         }
@@ -229,6 +286,14 @@ function calcBranchEndPosition(intersectionPoint, branchStations, positionType, 
         if (xDirection === 'RIGHT') {
             endPosition.x = canvas.width
         } else {
+            endPosition.x = 0
+        }
+    } else if (positionType === 'INTERSECTION') {
+        if (!direction || direction === 'RIGHT') {
+            endPosition.y = intersectionPoint.y
+            endPosition.x = canvas.width
+        } else if (direction === 'LEFT') {
+            endPosition.y = intersectionPoint.y
             endPosition.x = 0
         }
     }
---
+++ src/utils/canvas-utils.js
@@ -0,0 +1,35 @@
+export function drawRoundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius);
+    ctx.lineTo(x + radius, y + height);
+    ctx.arcTo(x, y + height, x, y + height - radius, radius);
+    ctx.lineTo(x, y + radius);
+    ctx.arcTo(x, y, x + radius, y, radius);
+    ctx.closePath();
+}
+
+export function drawRoundedLShape(_ctx, color, lineWidth, from, to, config = {cornerRadius: 30}) {
+    const {cornerRadius} = config
+    const isRight = to.x > from.x; // 是否向右绘制
+    const midX = isRight ? from.x + cornerRadius : from.x - cornerRadius;
+    const midY = to.y - cornerRadius;
+    _ctx.beginPath();
+    _ctx.moveTo(from.x, from.y);
+
+    // 画竖线
+    _ctx.lineTo(from.x, midY);
+
+    // 画圆角
+    _ctx.arcTo(from.x, to.y, midX, to.y, cornerRadius);
+    // 画横线
+    _ctx.moveTo(midX, to.y);
+    _ctx.lineTo(to.x, to.y);
+
+    _ctx.strokeStyle = color;
+    _ctx.lineWidth = lineWidth;
+    _ctx.stroke();
+}
